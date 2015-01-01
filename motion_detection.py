4b6069251f54ad46507ba51ff9310ac4fbd983f7
Lisiecki <dennislisiecki@gmail.com>
2015-01-01 18:52:49 +0100

    Tab changes

--- motion_detection.py
+++ motion_detection.py
@@ -35,7 +35,7 @@ class MyMotionDetector(object):
         # If there're more than 10 vectors with a magnitude greater
         # than 60, then say we've detected motion
         if (data > 60).sum() > 10:
-			returncode = subprocess.call(['sudo python3 pir.py'])
+            returncode = subprocess.call(['sudo python3 pir.py'])
             gpio.output(motion_detected_led, gpio.HIGH)
         else:
             gpio.output(motion_detected_led, gpio.LOW)

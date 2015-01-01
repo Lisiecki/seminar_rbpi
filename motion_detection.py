64cfb5554bcec858b8eb7025a1005274f351d8a3
clueseau <kuehlar@gmx.de>
2015-01-01 19:47:57 +0100

    Scheiss

--- motion_detection.py
+++ motion_detection.py
@@ -4,8 +4,9 @@ from __future__ import division
 import picamera
 import numpy as np
 import RPi.GPIO as gpio
-import time
+import time, sys
 import subprocess
+import pir
 
 motion_dtype = np.dtype([
     ('x', 'i1'),
@@ -14,6 +15,10 @@ motion_dtype = np.dtype([
     ])
 
 motion_detected_led = 40
+cmd = 'python3 pir.py'
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(7, GPIO.RISING)
+GPIO.add_event_callback(7, motion)
 
 class MyMotionDetector(object):
 
@@ -22,6 +27,10 @@ class MyMotionDetector(object):
         self.cols = (width + 15) // 16
         self.cols += 1 # there's always an extra column
         self.rows = (height + 15) // 16
+
+    def motion(pin):
+        print ("Bewegung erkannt")
+        return
 
     def write(self, s):
         # Load the motion data from the string to a numpy array
@@ -35,8 +44,8 @@ class MyMotionDetector(object):
         # If there're more than 10 vectors with a magnitude greater
         # than 60, then say we've detected motion
         if (data > 60).sum() > 10:
-            returncode = subprocess.call(['sudo python3 pir.py'])
             gpio.output(motion_detected_led, gpio.HIGH)
+            
         else:
             gpio.output(motion_detected_led, gpio.LOW)
         # Pretend we wrote all the bytes of s

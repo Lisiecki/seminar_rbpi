f60c219acf481142c703e497a8f4d5be23d28bf8
Lisiecki <dennislisiecki@gmail.com>
2015-02-08 17:44:19 +0100

    added socket timeout in codis

--- codis.py
+++ codis.py
@@ -18,21 +18,21 @@ MSG_INDEX_CMD = 0x0
 MSG_INDEX_POS = 0x1
 MSG_INDEX_OTHER = 0x2
 
-MSG_SHUTDOWN_CAM = 0x0
-MSG_MOTION_DETECTED = 0x1
-MSG_IDENTIFY = 0x2
-MSG_PAUSE_CAM = 0x3
-MSG_JOIN_CODIS = 0x4
-MSG_LEAVE_CODIS = 0x5
-MSG_JOIN_REQUEST = 0x6
-MSG_STATUS = 0x7
-MSG_ELECTION = 0x8
-MSG_COORDINATOR = 0x9
+SHUTDOWN_CAM_MSG = 0x0
+INTRUDER_MSG = 0x1
+JOIN_RESPONSE_MSG = 0x2
+PAUSE_CAM_MSG = 0x3
+JOIN_MSG = 0x4
+LEAVE_MSG = 0x5
+JOIN_REQUEST_MSG = 0x6
+STATUS_MSG = 0x7
+ELECTION_MSG = 0x8
+COORDINATOR_MSG = 0x9
 MSG_INTRUDER = 0xa
 
 UDP_IP = "<broadcast>"
 UDP_PORT = 58333
-MOTION_DETECTED_MSG = bytes([MSG_MOTION_DETECTED])
+MOTION_DETECTED_MSG = bytes([INTRUDER_MSG])
 PIR_DETECTED_MSG = bytes([0x2])
 PIR_GPIO = 7
 MOTION_DETECTED_THRESHOLD = 10
@@ -46,9 +46,12 @@ codis_list_pos = 0
 motion_cnt = 0
 no_motion_cnt = 0
 pir_event_enabled = 0
+
+# prepares the server socket to receive data from Codis system
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)   
 server_socket.bind(("", UDP_PORT))
+server_socket.settimeout(5.0)
 
 class MotionDetector(object):
 
@@ -108,28 +111,28 @@ def disable_pir():
     GPIO.remove_event_detect(PIR_GPIO)
     pir_event_enabled = 0
 
-def identify(addr):
-    identify_msg = bytes([MSG_IDENTIFY, codis_list_pos, codis_list_size])
-    server_socket.sendto(identify_msg, addr)
+def join_response(addr):
+    join_response_msg = bytes([JOIN_RESPONSE_MSG, codis_list_pos, codis_list_size])
+    server_socket.sendto(join_response_msg, addr)
 
 def request_join():
-    join_msg = bytes([MSG_JOIN_REQUEST, codis_list_pos, codis_list_size])
+    join_msg = bytes([JOIN_REQUEST_MSG, codis_list_pos, codis_list_size])
     server_socket.sendto(join_msg, (UDP_IP, UDP_PORT))
 
 def join(addr):
-    join_msg = bytes([MSG_JOIN_CODIS, codis_list_pos, codis_list_size])
+    join_msg = bytes([JOIN_MSG, codis_list_pos, codis_list_size])
     server_socket.sendto(join_msg, addr)
 
 def leave():
-    leave_msg = bytes([MSG_LEAVE_CODIS, codis_list_pos, codis_list_size])
+    leave_msg = bytes([LEAVE_MSG, codis_list_pos, codis_list_size])
     server_socket.sendto(leave_msg, (UDP_IP, UDP_PORT))
 
 def intruder_detected():
-    intruder_msg = bytes([MSG_INTRUDER, codis_list_pos, codis_list_size])
+    intruder_msg = bytes([INTRUDER_MSG, codis_list_pos, codis_list_size])
     server_socket.sendto(intruder_msg, (UDP_IP, UDP_PORT))
 
 def intruder_detected(pos):
-    intruder_msg = bytes([MSG_INTRUDER, codis_list_pos, codis_list_size])
+    intruder_msg = bytes([INTRUDER_MSG, codis_list_pos, codis_list_size])
     server_socket.sendto(intruder_msg, codis_list[pos])
 
 with picamera.PiCamera() as camera:
@@ -148,43 +151,50 @@ with picamera.PiCamera() as camera:
         request_join()
         wait = time.clock() + 5.0
         while time.clock() < wait:
-            remote_cmd, remote_addr = server_socket.recvfrom(4)
-            if remote_cmd[MSG_INDEX_CMD] == MSG_IDENTIFY:
-                print("join")
-                codis_list.insert(remote_cmd[MSG_INDEX_POS], remote_addr)
-                codis_list_pos += 1
-                join(remote_addr)
+            try:
+                remote_cmd, remote_addr = server_socket.recvfrom(4)
+                if remote_cmd[MSG_INDEX_CMD] == JOIN_RESPONSE_MSG:
+                    print("join")
+                    codis_list.insert(remote_cmd[MSG_INDEX_POS], remote_addr)
+                    codis_list_pos += 1
+                    join(remote_addr)
+            except (socket.timeout):
+                print("timeout")
+                break
 
         codis_list_pos += codis_list_size
         codis_list.append(remote_addr)
         codis_list_size = codis_list_pos + 1
 
         while 1:
-            remote_cmd, remote_addr = server_socket.recvfrom(4)
-            print(remote_cmd[MSG_INDEX_CMD])
-            if remote_cmd[MSG_INDEX_CMD] == MSG_SHUTDOWN_CAM:
-                break
-            elif remote_cmd[MSG_INDEX_CMD] == MSG_MOTION_DETECTED:
-                if pir_event_enabled == 0:
-                    pir_event_enabled = 1
-                    GPIO.add_event_detect(PIR_GPIO, GPIO.RISING)
-                    GPIO.add_event_callback(PIR_GPIO, motion)
-            elif remote_cmd[MSG_INDEX_CMD] == MSG_PAUSE_CAM:
-                break
-            elif remote_cmd[MSG_INDEX_CMD] == MSG_JOIN_CODIS:
-                print("HEEREEREE")
-            elif remote_cmd[MSG_INDEX_CMD] == MSG_LEAVE_CODIS:
-                codis_list.remove(remote_addr)
-                codis_list_size -= 1
-                if remote_cmd[MSG_INDEX_POS] < codis_list_pos:
-                    codis_list_pos -= 1
-            elif remote_cmd[MSG_INDEX_CMD] == MSG_JOIN_REQUEST:
-                print("identify")
-                codis_list.append(remote_addr)
-                codis_list_size += 1
-                identify(remote_addr)
-            elif remote_cmd[MSG_INDEX_CMD] == MSG_STATUS:
-                print("codis pos: ", codis_list_pos, '\n', "codis size: ", codis_list_size)
+            try:
+                remote_cmd, remote_addr = server_socket.recvfrom(4)
+                print(remote_cmd[MSG_INDEX_CMD])
+                if remote_cmd[MSG_INDEX_CMD] == SHUTDOWN_CAM_MSG:
+                    break
+                elif remote_cmd[MSG_INDEX_CMD] == INTRUDER_MSG:
+                    if pir_event_enabled == 0:
+                        pir_event_enabled = 1
+                        GPIO.add_event_detect(PIR_GPIO, GPIO.RISING)
+                        GPIO.add_event_callback(PIR_GPIO, motion)
+                elif remote_cmd[MSG_INDEX_CMD] == PAUSE_CAM_MSG:
+                    break
+                elif remote_cmd[MSG_INDEX_CMD] == JOIN_MSG:
+                    print("HEEREEREE")
+                elif remote_cmd[MSG_INDEX_CMD] == LEAVE_MSG:
+                    codis_list.remove(remote_addr)
+                    codis_list_size -= 1
+                    if remote_cmd[MSG_INDEX_POS] < codis_list_pos:
+                        codis_list_pos -= 1
+                elif remote_cmd[MSG_INDEX_CMD] == JOIN_REQUEST_MSG:
+                    print("identify")
+                    codis_list.append(remote_addr)
+                    codis_list_size += 1
+                    join_response(remote_addr)
+                elif remote_cmd[MSG_INDEX_CMD] == STATUS_MSG:
+                    print("codis pos: ", codis_list_pos, '\n', "codis size: ", codis_list_size)
+            except (socket.timeout):
+                print(timeout)
     except KeyboardInterrupt:
         leave()
         camera.stop_recording()
